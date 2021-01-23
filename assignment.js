fa377a331261dabcc7b4dc2e7162c95d1d6ba112
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-01-23 21:05:12 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,11 @@
+// https://github.com/omar-faruk02/assignment-js
+
+// KilometerToMeter
+
+function KilometerToMeter(kilometer)
+{
+  var meter=kilometer*1000;
+  return meter;
+}
+var result=KilometerToMeter(5);
+console.log(result);
